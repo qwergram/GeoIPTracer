49793795604995e9e28d26581babda4be041eae8
Norton Pengra <npengra317@gmail.com>
2016-04-17 16:49:10 -0700

    Created initial view

--- geoip/geoip/urls.py
+++ geoip/geoip/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from miner.views import index_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index_view),
 ]
