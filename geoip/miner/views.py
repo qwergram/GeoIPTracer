2e2dd7f68f6fcf337f14a6d074f7e14273eadf91
Norton Pengra <npengra317@gmail.com>
2016-04-17 19:53:54 -0700

    Added geolocating data to query view

--- geoip/miner/views.py
+++ geoip/miner/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+import requests
+import socket
 
 # Create your views here.
 
+API_ENDPOINT = "http://ip-api.com/json/"
+
+
 def query(request, *args, **kwargs):
-    return HttpResponse(request.GET.get('url', "whoops!"))
+    url = request.GET.get('url')
+    if url:
+        url = url.replace("http://", "")
+        url = url.replace("https://", "")
+        ips = socket.gethostbyname_ex(url)[2]
+        data = []
+        for ip in ips:
+            api_data = requests.get(API_ENDPOINT + ip).json()
+            data.append({
+                "url": url,
+                "ip": ip,
+                "lat": api_data['lat'],
+                "lon": api_data['lon'],
+                "co": api_data['coountry']
+            })
+        return render(request, "miner/query.html", {"data": data})
+    raise Http404("url not included")
 
 
 def test(request):
