fdb46e5ce2bc2e4552f58ce4e6635d2e14039e17
Norton Pengra <npengra317@gmail.com>
2016-04-17 18:10:35 -0700

    New route for form submission

--- geoip/geoip/urls.py
+++ geoip/geoip/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from miner.views import index_view
+from miner.views import index_view, form_submission
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index_view),
+    url(r'^submit/$', form_submission)
 ]
