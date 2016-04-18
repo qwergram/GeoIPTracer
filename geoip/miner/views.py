ca7cd3d8a9a4775e79c29a3739916bf31dfcf325
Norton Pengra <npengra317@gmail.com>
2016-04-17 18:29:31 -0700

    Fixed GET issue
    You can't call request.GET.get(..) multiple times
    the second time you call it, it will return None.

--- geoip/miner/views.py
+++ geoip/miner/views.py
@@ -8,6 +8,7 @@ def index_view(request, *args, **kwargs):
     return render(request, "miner/index.html", {})
 
 def form_submission(request, *args, **kwargs):
-    if request.POST.get('url', False):
-        return render(request, "miner/results.html", {"url": request.GET.get('url')})
+    url = request.GET.get('url', False)
+    if url:
+        return render(request, "miner/results.html", {"url": url})
     return redirect("index")
