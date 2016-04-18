923a28352fdd00e3f8ba130ad7525c5e7392c538
Norton Pengra <npengra317@gmail.com>
2016-04-18 11:51:18 -0700

    New GeoIP models

--- geoip/miner/models.py
+++ geoip/miner/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+
+    class Meta:
+        unique_together = ("lat", "lon")
+
+    city = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    zipcode = models.IntegerField()
+
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    isp = models.CharField(max_length=255)
+    org = models.CharField(max_length=255)
+
+
+class IP_Geo(models.Model):
+    ip = models.GenericIPAddressField(unique=True)
+    location = models.ForeignKey(Location)
+
+
+class URL_Log(models.Model):
+    url = models.URLField(unique=True)
+    ip = models.ForeignKey(IP_Geo)
